f453a9430aa257a807325502ee89e63850cd1f20
ct-jacksonchin <164849641+ct-jackonchin@users.noreply.github.com>
2025-12-30 18:00:50 +0800

    芟屏

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+
+<meta name="description" content="「SIERRA SEA」座落海映路 8 號，為新地歷來最大型私人住宅項目#發展板圖之頭炮，由4座大樓組成，提供 781個優質住宅單位，現正申請預售樓花同意書。「SIERRA SEA」布局座向匠心獨運，大部份單位坐擁廣闊海景或翠綠山巒景致，部分單位更享西沙全新綜合地標「GO PARK」 景觀，貼心滿足不同住戶的需求。">
+    <meta property="og:description" content="「SIERRA SEA」座落海映路 8 號，為新地歷來最大型私人住宅項目#發展板圖之頭炮，由4座大樓組成，提供 781個優質住宅單位，現正申請預售樓花同意書。「SIERRA SEA」布局座向匠心獨運，大部份單位坐擁廣闊海景或翠綠山巒景致，部分單位更享西沙全新綜合地標「GO PARK」 景觀，貼心滿足不同住戶的需求。">
+
+        <meta property="og:image" content="./image/imageyo.jpg">
+ 
+
+        <!-- Required OG Tags for correct display -->
+    <meta property="og:title" content="Sierra Sea | 歡迎預約參觀示範單位">
+    <meta property="og:url" content="https://www.sierraseawest.com/">
+    <meta property="og:type" content="website">
+
 <script type="text/javascript">
     // 立即偵測並跳轉
     if (/Android|iPhone|iPad|iPod/i.test(navigator.userAgent)) {
